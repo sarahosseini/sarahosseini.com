47b6c847472de29a2f34a8a336c3548a40ae7609
Sara Hosseini <s48hosseini@uwaterloo.ca>
2020-10-08 18:50:55 -0400

    Updates

---
+++ precache-manifest.5296933bc6b5e86baa18049c1c2ea1cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1994ad85cd532cb847a7d6f9a9ceb6e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3741472bd99108b13cc",
+    "url": "/static/css/main.b30dd722.chunk.css"
+  },
+  {
+    "revision": "7c3998b4b04e471182f3",
+    "url": "/static/js/2.3e104423.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3e104423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3741472bd99108b13cc",
+    "url": "/static/js/main.ee77b021.chunk.js"
+  },
+  {
+    "revision": "83d861f9b783c604af74",
+    "url": "/static/js/runtime-main.068ff63e.js"
+  },
+  {
+    "revision": "edd9f270f9452d7753d60d9ebd026371",
+    "url": "/static/media/aboutme.edd9f270.jpg"
+  }
+]);
